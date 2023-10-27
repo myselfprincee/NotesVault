5a746dc39b0379709cfeff1230d7d39be7f817cd
Prince Gupta <prince.kumar01129@gmail.com>
2023-10-27 19:54:15 +0530

    Added Links to Footer and Styling Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className='footer-section' >
       <span >
         <div className='footer-cont'>
-        <h3>Watch Our Socials</h3>
-        <p>Instagram</p>
-        <p>Discord</p>
-        <p>Github</p>
+          <h3>Watch Our Socials</h3>
+          <p>Instagram</p>
+          <p>Discord</p>
+          <p>Github</p>
         </div>
-        <div className='hr'/>
+        <div className='hr' />
         <div className='footer-cont'>
           <h3>Contact Me</h3>
-          <p>princegupta@duck.com</p>
-          <p>9871890758</p>
-          <p>www.princegupta.dev</p>
+          <Link to={"mailto:https://blogs.princegupta.me"} target='_blank' className='footer-Links'><p>princegupta@duck.com</p></Link>
+          <Link to={"tel:9871890758"} target='_blank' className='footer-Links'><p>9871890758</p></Link>
+          <Link to={"https://blogs.princegupta.me"} target='_blank' className='footer-Links'>www.princegupta.me</Link>
 
         </div>
         <div className='last-footer-cont'>
-        <div className='hr'/>
-        <div className='footer-cont'>
-        <h3>Watch Our Socials</h3>
-        <p>Instagram</p>
-        <p>Discord</p>
-        <p>Github</p>
+          <div className='hr' />
+          <div className='footer-cont'>
+            <h3>Watch Our Socials</h3>
+            <p>Instagram</p>
+            <p>Discord</p>
+            <p>Github</p>
+          </div>
         </div>
-        </div>
-        
+
 
       </span>
-      <div style={{display:"flex", alignItems:"center", justifyContent:"space-evenly"}}>
-      <p className='copyright'>© www.notesvault.co | All Rights Reserved.</p>
-      <p className='year'>2023</p>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-evenly" }}>
+        <p className='copyright'>© Notesvault  |  All Rights Reserved.</p>
+        <p className='year'>{currentYear}</p>
       </div>
-      
-      
+
+
     </footer>
   )
 }
